cd6393ad9a0301c2e4101618b0d07f001139b810
Sanaya Arya <sanayaarya0123@gmail.com>
2022-12-24 16:44:36 +0530

    completed

---
+++ live-code-editor/src/components/App.js
@@ -0,0 +1,15 @@
+import Main from './Main';
+
+function App() {
+    return (
+        <div className="">
+            <header className="">
+                <h1 className="">Code Editor</h1>
+                <p>You can write and Preview HTML and CSS code live!</p>
+            </header>
+            <Main />
+        </div>
+    )
+}
+
+export default App;
